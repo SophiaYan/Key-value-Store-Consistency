c606ad0c500dbd0142082d1a2db0ca121014d8c5
xiao chen <xiaochen@xiaos-MacBook-Air.local>
2016-03-27 00:00:13 -0500

    setting up framework

--- src/client/Client.java
+++ src/client/Client.java
@@ -1,5 +1,48 @@
 package client;
 
-public class Client {
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
 
+public class Client {
+	BufferedReader in;
+    PrintWriter out;
+    Socket socket;
+    
+    
+    
+    public Client(String serverAddr, int serverPort) throws IOException {
+		socket = new Socket(serverAddr, serverPort);
+		in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+	    out = new PrintWriter(socket.getOutputStream(), true);
+	}
+
+
+    private void run() throws IOException {    
+    	try (
+    			BufferedReader stdIn = new BufferedReader(new InputStreamReader(System.in));
+    		) 
+    	{
+    		System.out.println("Now you can type below: ");
+        	String userInput;
+        	while ((userInput = stdIn.readLine()) != null) {
+        		out.println(userInput);
+        	}
+    	}
+    	socket.close();
+    }
+    
+
+	public static void main(String[] args) throws NumberFormatException, IOException {
+    	if (args.length != 2) {
+			System.out.println("Usage: java program serverAddr serverPort");
+        	System.exit(0);
+		}
+    	
+    	Client c = new Client(args[0], Integer.parseInt(args[1]));
+    	c.run();
+    	
+    }
 }
