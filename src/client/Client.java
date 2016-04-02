b9f24496e9f9d1b2f0c3827027bce09b67e1e710
sophiayan <sophiaxueting@gmail.com>
2016-04-02 15:14:07 -0500

    Add client input parser and send functions

--- src/client/Client.java
+++ src/client/Client.java
@@ -1,10 +1,13 @@
 package client;
 
 import java.io.BufferedReader;
+import java.io.FileReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.Socket;
+import java.util.HashMap;
+import java.util.concurrent.TimeUnit;
 
 public class Client {
 	
@@ -14,21 +17,74 @@ public class Client {
 	private int consistency;  // indicate the consistency model
 	private int writeNum;
 	private int readNum;
+	private int serverId;
+	private boolean isFinished;  // indicate whether each operation has finished or not, true for has finished, false for not
+	
+    private HashMap<Integer, String> ipMap;
+    private HashMap<Integer, Integer> portMap; 
     
     /*
      *  client constructor
      */
-    public Client(String serverAddr, int serverPort) throws IOException {
-		socket = new Socket(serverAddr, serverPort);
-		socketInput = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-		socketOutput = new PrintWriter(socket.getOutputStream(), true);
+    public Client(int serverId) throws IOException {
+    	ipMap = new HashMap<Integer, String>();
+    	portMap = new HashMap<Integer, Integer>();
+    	readConfig("/Applications/eclipse-epsilon-1.2-macosx-cocoa-x86_64/CS425MP2/Key-value-Store-Consistency");
+    	
+    	//this.serverId = serverId;
+		boolean isSucceed; 
+		do {
+			isSucceed = makeConnection(serverId);
+			serverId = (serverId + 1) % 9;
+		} while (!isSucceed);
+    	
+		this.serverId = serverId - 1;
 		consistency = 0;
 		writeNum = 0;
 		readNum = 0;
+		isFinished = true;
+		
 	}
+    
+    private boolean makeConnection(int serverId) {
+    	try {
+    		socket = new Socket(ipMap.get(serverId), portMap.get(serverId));
+    		socketInput = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+    		socketOutput = new PrintWriter(socket.getOutputStream(), true);
+    		return true;
+    	} catch (Exception e){
+    		return false;
+    	}
+    }
+    
+    /**
+     * This is a helper function to read the config file and store corresponding info properly
+     * @param config
+     */
+    private void readConfig(String config){
+    	// Read and store process info from config file
+    	try (BufferedReader configReader = new BufferedReader(new FileReader(config))) {
 
+    		String currLine = configReader.readLine();		
+    		
+    		while ((currLine = configReader.readLine()) != null) {
+    			
+					String[] processInfo = currLine.split(" ", 3);
+					int id = Integer.parseInt(processInfo[0]);
+					String ip = processInfo[1];
+					int port = Integer.parseInt(processInfo[2]);
 
-    private void run() throws IOException {    
+					ipMap.put(id, ip);
+					portMap.put(id, port);
+					
+			}
+    	} catch (IOException e) {
+			e.printStackTrace();
+		}
+    }
+	
+
+    private void run() throws IOException, InterruptedException {    
     	try (
     			BufferedReader stdIn = new BufferedReader(new InputStreamReader(System.in));
     		) 
@@ -59,21 +115,89 @@ public class Client {
         	}
         	
         	while ((userInput = stdIn.readLine()) != null) {
-        		
+        		if (userInput.indexOf("get") == 0 && userInput.length() == 5){
+        			sendRequest(userInput);
+        		} else if ((userInput.indexOf("put") == 0) && (userInput.length() == 7)){
+        			sendRequest(userInput);
+        		} else if (userInput.equals("dump")){
+        			sendRequest(userInput);
+        		} else if (userInput.indexOf("delay") == 0){
+        			delay(userInput);
+        		} else {
+        			System.out.println("Wrong instruction.\n");
+        		}
         		//out.println(userInput);
         	}
     	}
     	socket.close();
     }
     
+    private void sendRequest(String req){
+    	
+//    	while(!isFinished){
+//    	};
+    	
+    	String [] tokens = req.split(" "); 	
+    	String realRequest;
+    	if (tokens[0].equals("get")){
+    		realRequest = "g" + tokens[1];
+    	} else if (tokens[0].equals("dump")) {
+    		realRequest = "d";
+    	} else {
+    		realRequest = "p" + tokens[1] + tokens[2];
+    	}
+    	
+    	try{
+    		socketOutput.println(realRequest);
+    	} catch (Exception e) {
+    		// this is to handle connection lost
+    		
+    		int newServerId = (serverId + 1) % 9;
+    		
+    		try {
+    			
+    			socket = new Socket(ipMap.get(newServerId), portMap.get(newServerId));
+    			socketInput = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+    			socketOutput = new PrintWriter(socket.getOutputStream(), true);
+    			serverId = newServerId;
+    			isFinished = false;
+    			
+    		} catch (IOException IOe) {
+    			
+    			boolean isSucceed; 
+    			do {
+    				newServerId = (newServerId + 1) % 9;
+    				isSucceed = makeConnection(newServerId);
+    			} while (!isSucceed);
+    			
+    			// update info
+    			serverId = newServerId;
+    			socketOutput.println("writeNum" + writeNum);
+    			socketOutput.println("readNum" + readNum);
+    			isFinished = false;
+    		}
+    		
+    	}
+    	
+    	//  reading (you'll get -1 as return value) or writing (an IOException (broken pipe) will be thrown) 
+    	//try catch if connection is lost
+    }
+    
 
-	public static void main(String[] args) throws NumberFormatException, IOException {
+    private void delay(String req) throws InterruptedException {
+    	String [] tokens = req.split(" ");
+    	int delayTime = Integer.parseInt(tokens[1]);
+    	TimeUnit.MILLISECONDS.sleep((long) delayTime);
+    }
+    
+
+	public static void main(String[] args) throws NumberFormatException, IOException, InterruptedException {
     	if (args.length != 2) {
 			System.out.println("Usage: java program serverAddr serverPort");
         	System.exit(0);
 		}
     	
-    	Client c = new Client(args[0], Integer.parseInt(args[1]));
+    	Client c = new Client(Integer.parseInt(args[1]));   // take the id of the server as argument
     	c.run();
     	
     }
--- src/server/ReplicaServer.java
+++ src/server/ReplicaServer.java
@@ -1,6 +1,8 @@
 package server;
 
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.net.ServerSocket;
 import java.io.FileReader;
@@ -75,6 +77,74 @@ public class ReplicaServer {
 		System.out.println("Bounds of delay in milliSec: " + minDelay + " " + maxDelay);
     }
 	
+    /*
+     * Write client request to the log file of the replicaserver
+     */
+    private void writeLog(String logMessage) throws IOException{
+    	BufferedWriter logWriter = null;
+    	try  
+    	{
+    	    FileWriter fstream = new FileWriter("out" + processId + ".txt", true); //true tells to append data.
+    	    logWriter = new BufferedWriter(fstream);
+    	    logWriter.write(logMessage);
+    	}
+    	catch (IOException e)
+    	{
+    	    System.err.println("Error: " + e.getMessage());
+    	}
+    	finally
+    	{
+    	    if(logWriter != null) {
+    	    	logWriter.close();
+    	    }
+    	}
+    }
+    
+    
+//	/**
+//	 * unicast Send msg to process id
+//	 * @param id
+//	 * @param msg
+//	 * @throws IOException
+//	 */
+//    private void unicast(int id, String realMsg, String type) throws IOException {
+//
+//    	Socket socket;
+//    	if(id == seqId){
+//    		socket = new Socket("localhost", seqPort);
+//    	}else{
+//    		socket = new Socket(ipMap.get(id), portMap.get(id));
+//    	}
+//
+//        try(PrintWriter out = new PrintWriter(socket.getOutputStream(), true)){
+//        	if(type == "cosend"){
+//        		String timeStampStr = timeStamp.toString();
+//        		out.println(type + " " + processId + " " + timeStampStr + " " + realMsg); 
+//			}
+//        	else{
+//        		out.println(type + " " + processId + " " + sendCounter + " " + realMsg);      //processId + timestamp + msg
+//        	}
+//        	System.out.println("Sent "+ realMsg +" to process "+ id +", system time is ­­­­­­­­­­­­­"+ System.currentTimeMillis());
+//        }
+//        finally{
+//        	 socket.close();
+//        }
+//       
+//    }
+    
+    
+//    private void multicast(String realMsg, String type) {
+//    	for(int id : ipMap.keySet()) {
+//    		try {
+//				unicast(id, realMsg, type);
+//			} catch (IOException e) {
+//				e.printStackTrace();
+//			}
+//    	}
+//    }
+    
+    
+    
     private void run() throws IOException {
     	
     	readConfig(config);
